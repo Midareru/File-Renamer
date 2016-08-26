f5a2296e488e178a39c9d1116cefe0b4db007b91
Midareru <tomlawn555@gmail.com>
2016-08-26 16:55:55 +1200

    Fixed image renaming, and added options to remove the ad image.

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import os
 import imghdr
 
 def main():
+	remove_ad = True # Remove the extra image ad at the start of each chapter.
 	path = "./files"
 	count = 1
 
+	# Renaming files
 	for root, dirs, files in os.walk(path):
 		for i in files:
 			img_type = imghdr.what(os.path.join(root, i))
-			os.rename(os.path.join(root, i), os.path.join(root, str(count) + "." + img_type))
-			count += 1
+			if i == '0' and remove_ad == True:
+				os.remove(os.path.join(root, i)) # Delete ad
+			else:
+				# Add image extension
+				os.rename(os.path.join(root, i), os.path.join(root, i + "." + img_type))
 
-	count = 1
+	# Renaming folders
 	for root, dirs, files in os.walk(path):
 		for i in dirs:
 			os.rename(os.path.join(root, i), os.path.join(root, str(count)))
