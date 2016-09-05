a4ab7ef3cd8cf4cf0ae0b96710737b7be565d5f4
Midareru <tomlawn555@gmail.com>
2016-09-05 18:37:33 +1200

    Now handles non numeric filenames

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import os
 import imghdr
+import sys
+
+# Returns an input string, with non digit chars removed
+def get_numeric(input_text):
+	text = ""
+	for c in input_text:
+		if c.isdigit():
+			text += c
+
+	return text
+
+
+# Renames folders to names with only numerals
+def clean_folders(path):
+	for root, dirs, files in os.walk(path):
+		for i in dirs:
+			try:
+				name = get_numeric(i)  # Strip non-numeric chars
+				os.rename(os.path.join(root, i), os.path.join(root, name))
+			except OSError:
+				print("Failed to rename: " + i)
+
+	print("Finished cleaning folders.")
 
 
 def main():
-	remove_ad = True # Remove the extra image ad at the start of each chapter.
+	remove_ad = True  # Remove the extra image ad at the start of each chapter.
 	path = "./files"
-	log_path = "log.txt" # Store corupted file list
+	log_path = "log.txt"  # Store corrupted file list
 	count = 1
 	corrupted = False
-
-	# Clear log file, and set it for read/write
-	logfile = open(log_path, "wr")
 
 	# Renaming folders
 	for root, dirs, files in os.walk(path):
@@ -21,25 +41,45 @@ def main():
 					os.rename(os.path.join(root, i), os.path.join(root, str(count)))
 				except OSError:
 					print("Failed to rename folder: " + os.path.join(root, i) + "\n")
-				count += 1	
+				count += 1
 		except ValueError:
 			print("Can't sort non-numeric folder names, operation failed.")
-			return
+			print("sanitise folder names? (y/n)")
+
+			clean = ""
+
+			# For comparability reasons, find out if we use input() or raw_input()
+			if sys.version_info[0] < 3:
+				clean = raw_input()
+			else:
+				clean = input()
+
+			if "y" in clean:
+				print("Removing non numerics from folder names")
+				clean_folders(path)  # Makes folders numeric only
+				print("Trying again...")
+				return main()  # Use recursion to try again
+			else:
+				print("Exiting.")
+				return False
+
+	# Clear log file, and set it for read/write
+	logfile = open(log_path, "w")
 
 	# Renaming files
 	for root, dirs, files in os.walk(path):
 		for i in files:
 			img_type = imghdr.what(os.path.join(root, i))
-			
-			if i == '0' and remove_ad == True:
+
+			if i == '0' and remove_ad is True:
 				try:
-					os.remove(os.path.join(root, i)) # Delete ad
+					os.remove(os.path.join(root, i))  # Delete ad
 				except OSError:
 					print("failed to delete: " + os.path.join(root, i) + "\n")
 			else:
 				# Add image extension
 				if img_type is None:
-					print("Possible corupted file: " + os.path.join(root, i))
+					print("Possible corrupted file: " + os.path.join(root, i))
 					logfile.write(os.path.join(root, i) + "\n")
 					corrupted = True
 				else:
@@ -54,11 +94,11 @@ def main():
 
 	# Close the log file, saving the contents
 	logfile.close()
+	print("done")
 	return corrupted
 
 
 corrupted = main()
-print "Done."
 
 # Prompt the user to use the repair tool if we had corrupt files
 if corrupted:
